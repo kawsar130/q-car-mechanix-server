682252cd44b04cb53f8ee5cf14aa84e1cc72ce5e
kawsar130 <kawsarahmed130@gmail.com>
2021-10-28 23:20:25 +0600

    Added port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
@@ -15,59 +15,59 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ihos6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 async function run() {
-  try {
-    await client.connect();
-    const database = client.db("carMechanic");
-    const servicesCollection = database.collection("services");
+    try {
+        await client.connect();
+        const database = client.db("carMechanic");
+        const servicesCollection = database.collection("services");
 
-    // Get API
-    app.get("/services", async (req, res) => {
-      const cursor = servicesCollection.find({});
-      const services = await cursor.toArray();
-      res.send(services);
-    });
+        // Get API
+        app.get("/services", async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
 
-    // Get single service
-    app.get("/services/:id", async (req, res) => {
-      const id = req.params.id;
-      console.log("Getting specific service", id);
-      const query = { _id: ObjectId(id) };
-      const service = await servicesCollection.findOne(query);
-      res.json(service);
-    });
+        // Get single service
+        app.get("/services/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log("Getting specific service", id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        });
 
-    // Post API
-    app.post("/services", async (req, res) => {
-      const service = req.body;
-      console.log("Hit the post API: ", service);
+        // Post API
+        app.post("/services", async (req, res) => {
+            const service = req.body;
+            console.log("Hit the post API: ", service);
 
-      const result = await servicesCollection.insertOne(service);
-      console.log(result);
-      res.json(result);
-    });
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result);
+        });
 
-    // Delete API
-    app.delete("/services/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await servicesCollection.deleteOne(query);
-      res.json(result);
-    });
-  } finally {
-    // await client.close();
-  }
+        // Delete API
+        app.delete("/services/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        });
+    } finally {
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Running Q-Car Mechanix");
+    res.send("Running Q-Car Mechanix");
 });
 
 app.listen(port, () => {
-  console.log("Running Q-Car Mechanix on port " + port);
+    console.log("Running Q-Car Mechanix on port " + port);
 });
